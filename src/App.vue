7007b5ae31d91fa784af52b60fdf4c4454d7d48d
Fabian Adelmann <adelmann@hydra-newmedia.com>
2021-10-25 15:13:44 +0200

    basic setup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 /* eslint-disable no-unused-vars */
 <template>
   <div id="app">
-    <div class="characters">
-    <div class="character" v-for="character in ApiCharacters" :key="character.id">
-        <h1>{{ character.name }}</h1>
-        <p>{{ character.description }}</p>
-        <p>price: ${{ character.price }}</p>
-        <img :src="`${character.thumbnail.path}/portrait_xlarge.jpg`" alt="" />
+    <div class="hub">
+      <button @click="removeAll">Remove all</button>
+      <h1 class="budget">${{ budget }}</h1>
+      <div class="avengers">
+        <div class="avenger" v-for="avenger in avengers" :key="`avenger_${avenger.id}`">          
+          <img :src="`${avenger.thumbnail.path}/standard_xlarge.jpg`" :alt="avenger.name" />
+          <h5>{{ avenger.name }}</h5>
+        </div>
     </div>
+    </div>
+    <div class="characters">
+      <div v-for="character in apiCharacters" :key="character.id" class="character" :class="{ selected: character.selected}" @click="toggleSelect(character)">
+          <h1>{{ character.name }}</h1>
+          <p>price: ${{ character.price }}</p>
+          <a :href="character.urls[1].url" target="_blank">To the Wiki</a>
+          <div class="thumbnail">
+            <img :src="`${character.thumbnail.path}/standard_xlarge.jpg`" :alt="character.name" />
+          </div>
+          <button class="select-btn" :disabled="budget < character.price && !character.selected || avengers.length >= 6 && !character.selected">{{ character.selected === false ? 'Select' : 'Unselect' }}</button>
+      </div>
     </div>
   </div>
 </template>
@@ -22,7 +35,8 @@ export default {
   name: 'App',
   data() {
     return {
-      ApiCharacters: [],
+      apiCharacters: [],
+      budget: 20,
     }
   },
   mounted() {
@@ -32,11 +46,33 @@ export default {
       .then(response => {
         const charObj = response.data.data.results[0];
         charObj.price = element.price;
-        this.ApiCharacters.push(charObj)
+        charObj.selected = false;
+        charObj.btnText = 'Select';
+        this.apiCharacters.push(charObj)
           }
         )
       .catch(error => console.log(error));
     })
+  },
+  methods: {
+    toggleSelect(c) {
+        if(c.selected === false && this.budget >= c.price && this.avengers.length < 6) {
+          c.selected = true;
+          this.budget -= c.price;
+        } else if (c.selected === true) {
+          c.selected = false;
+          this.budget += c.price;
+        }
+    },
+    removeAll() {
+      this.budget = 20;
+      this.apiCharacters.map(e => e.selected = false);
+    }
+  },
+  computed: {
+    avengers() {
+      return this.apiCharacters.filter(c => c.selected === true);
+    }
   }
 }
 </script>
@@ -49,18 +85,103 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
 }
 
 .characters {
   display: flex;
   flex-wrap: wrap;
   width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 400px;
 }
 
-.character{
-  background: rgb(219, 217, 190);
-  padding: 2rem;
-  margin: 3rem;
-  width: calc(33% - 10rem);
+.character {
+  background: rgb(231, 231, 231);
+  border: solid 5px rgb(216, 216, 216);
+  border-radius: 10%;
+  padding: 1rem;
+  margin: 1rem;
+  min-width: 350px;
+  max-width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
 }
+
+.thumbnail {
+  border-radius: 12%;
+  transition: all ease-in-out 300ms;
+  box-shadow: 0 0 0 black;
+  margin: 40px 0;
+  height: 200px;
+}
+
+.thumbnail img {
+  border-radius: 12%;
+  height: 200px;
+}
+
+.character:hover .thumbnail {
+  transform: translate(-5px, -5px);
+  box-shadow: black;
+  box-shadow: 20px 20px 0 black;
+}
+
+
+.selected {
+  background: rgb(255, 249, 169);
+  border: solid 5px black;
+}
+
+
+.avenger-thubmnail{
+  border-radius: 50%;
+  width: 100px;
+}
+
+.hub {
+  position: fixed;
+  background: black;
+  color: white;
+  min-height: 200px;
+  min-width: 60%;
+  bottom: 0;
+  left: calc(50% - 30%);
+  box-shadow: 11px -7px 11px 3px #0000004f;
+  z-index: 10;
+}
+
+.avengers {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 0 3rem;
+}
+
+.avenger {
+  margin: 10px;
+}
+
+.avenger img{
+  border-radius: 50%;
+  width: 100px;
+}
+
+.select-btn {
+  cursor: pointer;
+  all: unset;
+  background: yellow;
+  padding: .5rem 1rem;
+}
+
+.select-btn:disabled {
+  opacity: .3;
+}
+
 </style>
